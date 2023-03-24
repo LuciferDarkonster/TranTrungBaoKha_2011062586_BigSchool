bb8f8cfff4b69a08e739952d74975dccdbd1f609
LuciferDarkonster <trantrungbaokha@gmail.com>
2023-03-24 16:12:16 +0700

    Đả hoàn thành chức năng tuần 3

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using BigSchool.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,25 @@ namespace BigSchool.Controllers
         public CoursesController()
         {
             _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var course = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = course,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
         [Authorize]
         public ActionResult Create()
